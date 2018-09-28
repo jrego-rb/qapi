e910844b2307079d1b2e0f00b2ffd18ffef1c608
Juan Ignacio Rego <JuanIgnacio.Rego@gmail.com>
2018-09-28 16:54:37 -0300

    DECD-3463. Ahora, el recurso es /vep/unNúmeroDeVepCualquiera. Se realiza un GET a /vep/xxx y se devolverá un JSON con un VEP genérico. Mientras no se haya utilizado el número previamente, se podría transaccionar exitosamente.

--- qapi.js
+++ qapi.js
@@ -561,7 +561,7 @@ function checkRedis(req, res, next) {
 	});       	
 }
 
-function vepNumber390229689(req, res, next) {
+function vepGenerator(req, res, next) {
 	var vep = { "payment":{ "amount":"30000", "currency":"ARS", "installments":1 }, "ticket_payment":{ "cuit":{ "authorizing":"20240215455", "taxpayer":"20240215455", "user":"20240215455", "owner":"20240215455" }, "cp":{ "payment_entity":"1002", "payer_bank":"389", "payment_format":1, "branch_office_type":8 }, "vep":{ "number":"000292454419", "advance_fee":"0", "fiscal_period":"201703", "concept":"19", "sub_concept":"19", "establishment":"0", "payment_description_extract":"MONOTR03/17", "payment_description":"Monotributo - Pago Mensual - Personas Fisicas", "payment_type":"1", "form":"152", "expiration_date":"2017-12-07 18:42:00", "creation_date":"2017-12-07 18:42:00", "due_date":"2017-12-07 18:42:00", "collecting_agency":"organismo recaudador", "description_concept":"sub con", "subconcept_description":"subconcept description", "owner_transaction_id":"asdf", "field_escription":"field_escription", "content_description":"content_description", "tax_description":"Tax description", "taxes":[ { "id":20, "amount":368 }, { "id":21, "amount":399.3 }, { "id":24, "amount":419 } ], "content":"1502021", "field_id":"3", "field_type":"N" } } };
 	res.send(200, vep);
 }
@@ -597,7 +597,7 @@ server.del('/sites/mpos', unsetMPOS);
 
 // Ruta para reportar el último error via HTTP
 server.get('/healthcheck', version);
-server.get('/vep/390229689', vepNumber390229689);
+server.get('/vep/:vepNumber', vepGenerator);
 
 server.listen('8080', function() {
 	console.log('%s listening at %s', server.name, server.url);
