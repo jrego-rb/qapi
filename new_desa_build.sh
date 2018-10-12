83627cfe0fe6f71e0ed901dc4b7b3bd301542120
Juan Ignacio Rego <JuanIgnacio.Rego@gmail.com>
2018-10-12 14:52:17 -0300

    Agregado un nuevo build que apunta a la registry del nuevo DESA

---
+++ new_desa_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#IMG="lapp-dvde004:5000/qapi:$(git describe --abbrev=0 --tags 2>/dev/null || echo 'latest')"
+IMG="registry-desa.prismamediosdepago.com/decidir2/qapi:$(git describe --abbrev=0 --tags 2>/dev/null || echo 'latest')"
+
+echo "Building $IMG"
+docker build -t "$IMG" . && \
+docker push "$IMG"
